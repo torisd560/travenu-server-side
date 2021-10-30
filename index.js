1d81adca1bb911a391f4b6f7f0853683f80ad5bb
torisd560 <mitalisd123@gmail.com>
2021-10-30 22:07:53 +0600

    added updates

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000
 
 // middleWare
 app.use(cors())
-app.use(express.json())
+app.use(express.json()) 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruilc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri)
